2fa6cb282fbd2088c33e4d47d954c5ef7ce111fc
Blackenor <seba.metalhead@gmail.com>
2022-10-11 18:55:34 -0300

    Creación de los controladores y las vistas del menu de administración

---
+++ Safe Core/Controllers/AdministracionController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Safe_Core.Controllers
+{
+    public class AdministracionController : Controller
+    {
+        // GET: Administracion
+        public ActionResult Actividades()
+        {
+            return View();
+        }
+
+        public ActionResult CalculoA()
+        {
+            return View();
+        }
+
+        public ActionResult PlanificarVisita()
+        {
+            return View();
+        }
+
+        public ActionResult CrearCapacitacion()
+        {
+            return View();
+        }
+
+
+    }
+}

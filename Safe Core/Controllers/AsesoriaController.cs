33eaee5ec9b8372f7995a578ec7aca21cf0a1497
Blackenor <seba.metalhead@gmail.com>
2022-10-11 19:46:39 -0300

    Modificación del footer y creación del controlador y vistas del menú "Asesoría"

---
+++ Safe Core/Controllers/AsesoriaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Safe_Core.Controllers
+{
+    public class AsesoriaController : Controller
+    {
+        // GET: Asesoria
+        public ActionResult IngresarAsesoria()
+        {
+            return View();
+        }
+
+        public ActionResult AsesoriaEspecial()
+        {
+            return View();
+        }
+    }
+}

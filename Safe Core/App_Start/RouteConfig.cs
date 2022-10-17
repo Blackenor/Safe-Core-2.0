d9f3eb590e4a01fc63336be4e495fcdeea0b4d45
Blackenor <seba.metalhead@gmail.com>
2022-10-16 23:54:15 -0300

    Modificación del login

--- Safe Core/App_Start/RouteConfig.cs
+++ Safe Core/App_Start/RouteConfig.cs
@@ -16,8 +16,8 @@ namespace Safe_Core
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                //defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
+                //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
             );
         }
     }

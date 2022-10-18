9912af2084997384dbadc7625215f060bf38d5a4
Doppelsito <maur.barriga@alumnos.duoc.cl>
2022-10-18 18:14:02 -0300

    Regla de negocio para rut y telefono (un maximo de 9 caracteres)

--- SafeCore.DAL/SafeCoreModel.Designer.cs
+++ SafeCore.DAL/SafeCoreModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\DesarrolloProyecto\Documents\Seba\Safe Core\SafeCore.DAL\SafeCoreModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Daekh\source\repos\Safe-Core-2.0\SafeCore.DAL\SafeCoreModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.

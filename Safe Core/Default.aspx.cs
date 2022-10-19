1310f34a127bfc1f44ff4396e58b22ea695d0a13
Doppelsito <maur.barriga@alumnos.duoc.cl>
2022-10-18 22:17:30 -0300

    maximo de caracteres para rut y telefono (9)

---
+++ Safe Core/Default.aspx.cs
@@ -0,0 +1,53 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Safe_Core
+{
+    public partial class _Default : Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            OracleConnection conexion = new OracleConnection("DATA SOURCE = SafeCoreEntities; PASSWORD= sc1234; USER ID = SAFECORE");
+            conexion.Open();
+            OracleCommand comando = new OracleCommand("SELECT * FROM USUARIOS WHERE NOMBRE = :nombre AND PASS = :pass", conexion);
+
+
+            comando.Parameters.Add(":nombre");
+            comando.Parameters.Add(":pass");
+
+            OracleDataReader lector = comando.ExecuteReader();
+
+            if (lector.Read())
+            {
+                if (lector["ROL_ID_ROL"].ToString() == "1")
+                {
+                    Server.Transfer("Index");
+                    conexion.Close();
+                }
+                if (lector["ROL_ID_ROL"].ToString() == "2")
+                {
+                    Server.Transfer("Index");
+                    conexion.Close();
+                }
+
+            }
+
+            else 
+            {
+
+            }
+
+        }
+    }
+}

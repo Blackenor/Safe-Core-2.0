30272ab77ec3f8641881af842dbc75257e1dd3d9
Doppelsito <maur.barriga@alumnos.duoc.cl>
2022-10-24 16:13:31 -0300

    Campo de prueba para calculo A (falta de resultado)

---
+++ Safe Core/Models/CalculadoraController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Safe_Core.Models;
+
+namespace Safe_Core.Models
+{
+    public class CalculadoraController : Controller
+    {
+        // GET: Calculadora
+        public ActionResult Calculator()
+        {
+            return View();
+        }
+    }
+
+    public class CalculadoraViewModel
+    {
+        public int Operador1 { get; set; }
+        public int Operador2 { get; set; }
+
+        public string Accion { get; set; }
+        public int Respuesta { get; set; }
+    }
+
+}

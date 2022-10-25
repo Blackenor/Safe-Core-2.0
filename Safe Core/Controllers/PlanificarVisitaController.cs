74884d6647e243afbc8e8e6adc3f8b2d2220fc03
Doppelsito <maur.barriga@alumnos.duoc.cl>
2022-10-24 21:46:47 -0300

    Planificar visita correccion

---
+++ Safe Core/Controllers/PlanificarVisitaController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SafeCore.BLL;
+
+namespace Safe_Core.Controllers
+{
+    public class PlanificarVisitaController : Controller
+    {
+        // GET: PlanificarVisita
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PlanificarVisita/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PlanificarVisita/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PlanificarVisita/Create
+        [HttpPost]
+        public ActionResult Create(VisitasTerreno visitasTerreno)
+        {
+            try
+            {
+                try
+                {
+                    visitasTerreno.Create();
+                    TempData["mensaje"] = "Visita Planificada correctamente";
+                    return View();
+                }
+                catch 
+                {
+                    TempData["mensaje"] = "Error al planificar la visita";
+                    return View();
+                }
+                // TODO: Add insert logic here
+
+                //return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PlanificarVisita/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PlanificarVisita/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PlanificarVisita/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PlanificarVisita/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
